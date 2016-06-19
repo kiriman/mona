342f9d78505bb63667eac9c8295b504f9808e020
Kiriman <sunny@yandex.ru>
2016-06-19 17:05:37 +1000

    first form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-console.log("1.js is included");
+console.log("main.js is started");
+
+function getFormData(){
+	var form = document.forms.feedback;
+	var inputName = form.elements.inputName;
+	var inputEmail = form.elements.inputEmail;
+	var inputText = form.elements.inputText;
+	var inputFile = form.elements.inputFile;	
+}
+
+function preview(){
+	console.log("preview is started");	
+	
+	console.log( inputName.value );
+	console.log( inputEmail.value );
+	console.log( inputText.value );
+}
