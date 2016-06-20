8f2c6d02c3d235f03ce3587acfeb81d56264bcfe
Kiriman <sunny@yandex.ru>
2016-06-21 00:55:40 +1000

    fix: make build

---
+++ build/api/1.php
@@ -0,0 +1,32 @@
+<?php
+$hostname = "m60mxazb4g6sb4nn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "k47xels8p95j4de2";
+$password = "ym3yolj7pmkr4md4";
+$database = "qwtyyvmxd9kj0xdi";
+$charset = "utf8";
+
+$dsn = "mysql:host=$hostname;dbname=$database;charset=$charset";
+$options = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+$pdo = new PDO($dsn, $username, $password, $options);
+
+
+function isAdmin($pdo){
+    $session_id = "123457";
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE session_id = :session_id LIMIT 1');
+    $stmt->execute(array('session_id' => $session_id));
+    $r = $stmt->fetch();
+    print_r($r);
+    if($r){
+    	echo "true";
+    }else{
+    	echo "false";
+    }
+
+}
+
+$t = isAdmin($pdo);
+echo json_encode($t);
+?>
